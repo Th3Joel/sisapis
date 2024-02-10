a4a19a44b12c7a1d3bffef5a9462ac6f493e7083
joel <joel@gmail.com>
2024-02-09 20:08:38 -0600

    sdf

--- sisventa/src/index.ts
+++ sisventa/src/index.ts
@@ -1,10 +1,12 @@
 import { Elysia } from "elysia";
 import AuhtR from "./routes/AuthR";
 import UserR from "./routes/UserR";
+import {cors} from '@elysiajs/cors'
 
 import db from "./config/Database";
 
 const app = new Elysia({prefix:'/sisventa'})
+.use(cors)
 
 .use(db)
 .use(AuhtR)
