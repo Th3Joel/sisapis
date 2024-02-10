9fb113c11fbe460d326a5185e4f3247b7470434a
joel <joel@gmail.com>
2024-02-10 13:17:36 -0600

    dg

--- start/index.ts
+++ start/index.ts
@@ -2,16 +2,25 @@ import { Elysia } from "elysia";
 
 import sisventa from "../sisventa/src/index";
 
-import {cors} from "@elysiajs/cors";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
-.use(cors())
+  .use(
+    cors({
+      origin: true, // Acepta solicitudes desde cualquier origen
+      methods: "*", // Permite todos los métodos HTTP
+      allowedHeaders: "*", // Acepta todas las cabeceras entrantes
+      exposedHeaders: "*", // Expone todas las cabeceras en la respuesta
+      credentials: true, // Permite el uso de credenciales (cookies, encabezados de autorización, etc.)
+      maxAge: 5,
+    })
+  )
 
-.use(sisventa)
+  .use(sisventa)
 
-.get("/", () => "Hola")
+  .get("/", () => "Hola")
 
-.listen(8080);
+  .listen(8080);
 
 console.log(
   `💀 server is running at ${app.server?.hostname}: ${app.server?.port} 📡`
