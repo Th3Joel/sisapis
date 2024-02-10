7c90bbdcc31e7e856d63245da5871eb6ebdde39e
joel <joel@gmail.com>
2024-02-10 13:33:54 -0600

    sdf

--- start/index.ts
+++ start/index.ts
@@ -7,12 +7,7 @@ import { cors } from "@elysiajs/cors";
 const app = new Elysia()
   .use(
     cors({
-      origin: true, // Acepta solicitudes desde cualquier origen
-      methods: "*", // Permite todos los métodos HTTP
-      allowedHeaders: "*", // Acepta todas las cabeceras entrantes
-      exposedHeaders: "*", // Expone todas las cabeceras en la respuesta
-      credentials: true, // Permite el uso de credenciales (cookies, encabezados de autorización, etc.)
-      maxAge: 5,
+      origin: "*"
     })
   )
 
